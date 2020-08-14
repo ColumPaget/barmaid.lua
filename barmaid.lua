472cacd418b33c39851c1d11bf11873dab33054d
ColumPaget <colums.projects@gmail.com>
2020-08-14 21:33:23 +0100

    only lookup usage of requested partitions

--- barmaid.lua
+++ barmaid.lua
@@ -543,11 +543,37 @@ LookupHWmon()
 end
 
 
+function LookupPartitionsGetList()
+local toks, str
+local parts={}
+
+toks=strutil.TOKENIZER(settings.display, "$(|:|)", "ms")
+str=toks:next()
+while str ~= nil
+do
+	if str=="$("
+	then
+		str=toks:next()
+		if str=="fs"
+		then
+		str=toks:next() --consume the ':'
+		str=toks:next()
+		parts[str]="y"
+		end
+	end
+	str=toks:next()
+end
+
+return parts
+end
+	
 
 function LookupPartitions()
 local str, perc, color, toks
-local S
+local S, requested_partitions
 
+
+requested_partitions=LookupPartitionsGetList()
 
 S=stream.STREAM("/proc/self/mounts", "r")
 if S ~= nil
@@ -560,7 +586,7 @@ then
 		fs_type=toks:next()
 		fs_mount=toks:next()
 
-		if fs_type ~= "none" and fs_type ~="cgroups"
+		if fs_type ~= "none" and fs_type ~="cgroups" and requested_partitions[fs_mount] ~= nil
 		then
 			perc=math.floor( (filesys.fs_used(fs_mount) * 100 / filesys.fs_size(fs_mount)) + 0.5)
 			AddDisplayValue("fs:"..fs_mount, perc, nil, usage_color_map)
